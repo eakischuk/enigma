c02c8e4cf6ff8d8326c042c72f8efc1eee048751
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-08 14:10:01 -0600

    Add date, split_date, and four_offsets methods

--- lib/shift_offset.rb
+++ lib/shift_offset.rb
@@ -1,5 +1,22 @@
+require 'date'
+
 class ShiftOffset
-  def initialize(date = Date.today)
+  attr_reader :date
+
+  def initialize(date = (Date.today.to_s.delete_prefix("20").split("-")).reverse.join)
     @date = date
+  end
+
+  def split_date
+    @date.split(//)
+  end
+
+  def four_offsets
+    offsets = {}
+    offsets["A"] = split_date[-4].to_i
+    offsets["B"] = split_date[-3].to_i
+    offsets["C"] = split_date[-2].to_i
+    offsets["D"] = split_date[-1].to_i
+    offsets
   end
 end
