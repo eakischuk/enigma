55c1caa0fd6829cf823bb382912c79d7b17b87b0
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-08 12:59:43 -0600

    Add test to split key, create split key hash, and key hash with integer values

--- spec/shift_key_spec.rb
+++ spec/shift_key_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe ShiftKey do
     expect(@shift_key2.shift_key).to eq("09562")
   end
 
+  it 'splits key into array' do
+    expect(@shift_key2.split_key).to eq(["0", "9", "5", "6", "2"])
+  end
+
+  it 'has hash of split keys' do
+    result = {"A" => ["0", "9"],
+              "B" => ["9", "5"],
+              "C" => ["5", "6"],
+              "D" => ["6", "2"]}
+    expect(@shift_key2.key_hash).to eq(result)
+  end
+
   it 'has four keys' do
     result = {"A" => 9,
               "B" => 95,
