884f09cba8c6d626f924359a99e33dc16d033874
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-08 14:09:39 -0600

    Add test for date method, split_date, and four_offsets

--- spec/shift_offset_spec.rb
+++ spec/shift_offset_spec.rb
@@ -1,12 +1,27 @@
 require './shift_offset'
+require 'date'
 require 'pry'
 
 RSpec.describe ShiftOffset do
   before(:each) do
-    @shift_offset = ShiftOffset.new("080721")
+    @shift_offset1 = ShiftOffset.new("080721")
+    @shift_offset2 = ShiftOffset.new
   end
 
   it 'exists and has attributes' do
-    expect(@shift_offset).to be_a(ShiftOffset)
+    expect(@shift_offset1).to be_a(ShiftOffset)
+    expect(@shift_offset1.date).to eq("080721")
+  end
+
+  it 'has split date' do
+    expect(@shift_offset1.split_date).to eq(["0", "8", "0", "7", "2", "1"])
+  end
+
+  it 'has four offsets' do
+    result = {"A" => 0,
+              "B" => 7,
+              "C" => 2,
+              "D" => 1}
+    expect(@shift_offset1.four_offsets).to eq(result)
   end
 end
