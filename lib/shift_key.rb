83194cdeecd03aea6e4bba9e48619ed23b0bbca2
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-08 13:00:14 -0600

    Add split_key, key_hash, and four_keys methods

--- lib/shift_key.rb
+++ lib/shift_key.rb
@@ -3,4 +3,27 @@ class ShiftKey
   def initialize(key = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"].sample(5).join)
     @shift_key = key
   end
+
+  def split_key
+    @shift_key.split(//)
+  end
+
+  def key_hash
+    hash = Hash.new {|hash, key| hash[key] = []}
+    hash["A"] << split_key[0]
+    hash["A"] << split_key[1]
+    hash["B"] << split_key[1]
+    hash["B"] << split_key[2]
+    hash["C"] << split_key[2]
+    hash["C"] << split_key[3]
+    hash["D"] << split_key[3]
+    hash["D"] << split_key[4]
+    hash
+  end
+
+  def four_keys
+    key_hash.transform_values do |value|
+      value.join.to_i
+    end
+  end
 end
