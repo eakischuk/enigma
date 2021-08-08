bf88e027fcc8b127b7fae2ae53f404e190ae34d9
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-07 21:46:41 -0600

    Add shift_key instance variable with optional argument and attr_reader

--- lib/shift_key.rb
+++ lib/shift_key.rb
@@ -1,5 +1,6 @@
 class ShiftKey
-  def initialize(key = ["0".."9"].sample(5))
+  attr_reader :shift_key
+  def initialize(key = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"].sample(5).join)
     @shift_key = key
   end
 end
