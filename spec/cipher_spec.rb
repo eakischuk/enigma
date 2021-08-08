30ad867a09cf94d5fe141c8898a8ab43cba66e49
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-08 17:06:03 -0600

    Add test for split message and message hash

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -12,5 +12,28 @@ RSpec.describe Cipher do
 
   it 'exists and has attributes' do
     expect(@cipher).to be_a(Cipher)
+    expect(@cipher.key).to eq(@shift_key)
+    expect(@cipher.offset).to eq(@shift_offset)
+  end
+
+  it 'has shifts' do
+    result = {"A" => 9,
+              "B" => 99,
+              "C" => 23,
+              "D" => 19}
+    expect(@cipher.shifts).to eq(result)
+  end
+
+  it 'splits message into four arrays' do
+    result = ["t", "h", "e", " ", "s", "t", "o", "n", "e", "d", " ", "a", "p", "e", " ", "h", "y", "p", "o", "t", "h", "e", "s", "i", "s", "."]
+    expect(@cipher.split_message).to eq(result)
+  end
+
+  it 'has message hash' do
+    result = {"A" => ["t", "s", "e", "p", "y", "h", "s"],
+      "B" => ["h", "t", "d", "e", "p", "e", "."],
+      "C" => ["e", "o", " ", " ", "o", "s", nil],
+      "D" => [" ", "n", "a", "h", "t", "i", nil]}
+    expect(@cipher.message_hash).to eq(result)
   end
 end
