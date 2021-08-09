96c1f3f2d231bf8d0864ab8d8d364e826ec84bbf
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-09 15:09:07 -0600

    Add runner files encrypt and decrypt

---
+++ decrypt.rb
@@ -0,0 +1,8 @@
+require './lib/enigma'
+enigma = Enigma.new
+cyphertext = File.read(ARGV[0])
+new_file = File.open(ARGV[1], "w")
+info = enigma.decrypt(cyphertext, ARGV[2].to_s, ARGV[3].to_s)
+new_file.write(info[:decryption])
+new_file.close
+p "Created #{new_file.path}, with the key #{info[:key]} and date #{info[:date]}"
---
+++ encrypt.rb
@@ -0,0 +1,9 @@
+require './lib/enigma'
+require 'pry'
+enigma = Enigma.new
+message = File.read(ARGV[0])
+new_file = File.open(ARGV[1], "w")
+info = enigma.encrypt(message)
+new_file.write(info[:encryption])
+new_file.close
+p "Created #{new_file.path}, with the key #{info[:key]} and date #{info[:date]}"
