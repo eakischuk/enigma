f96d8aa1dd9f1a22e4a0b003dc86e77485c3c108
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-09 15:08:21 -0600

    Delete extra spaces in encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,9 @@
+require 'date'
+require './lib/cipher'
 class Enigma
   def encrypt(message, key = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"].sample(5).join, date = (Date.today.to_s.delete_prefix("20").split("-")).reverse.join)
-      cipher = Cipher.new(message, key, date)
-      cipher.encrypted_message
+    cipher = Cipher.new(message, key, date)
+    cipher.encrypted_message
   end
 
   def decrypt(cyphertext, key, date = (Date.today.to_s.delete_prefix("20").split("-")).reverse.join)
